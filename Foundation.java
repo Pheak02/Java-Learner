e3eac1d6ea6a226fb85d1f3ff4c49cbd94d8a102
Pheak02 <email@gmail.com>
2023-01-21 01:11:12 +0700

    Foundation d

--- Foundation.java
+++ Foundation.java
@@ -2,7 +2,8 @@ public class Foundation { // Foundation is the name of the class
     public static void main(String[] args) { // main is the name of the fucntion
         System.out.println("Hello there!");// statement - member of the method
         // access member by using .out.
-        System.out.println("Hello" + args[0]);// pass the argument that the user can type.
+        // System.out.println("Hello" + args[0]);// pass the argument that the user can
+        // type.
         // args[0]​​​ refers to index 0 of the array that we add on to the statement.
     }
 
