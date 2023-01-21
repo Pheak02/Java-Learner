23870f6336cb496020bd8f3b0d95de87cfc8542e
Pheak02 <email@gmail.com>
2023-01-21 16:55:38 +0700

    Very Foundamental of Array

--- Array.java
+++ Array.java
@@ -2,6 +2,6 @@ public class Array {
     public static void main(String[] args) {
         int[] grades = new int[10];
         grades[0] = 10;
-        System.out.println(grades);
+        System.out.println(grades[0]);
     }
 }
