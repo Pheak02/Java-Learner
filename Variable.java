8e9feb815c12c33a5e8bfd0da03fdb8c97fe42f9
Pheak02 <email@gmail.com>
2023-01-21 10:37:38 +0700

    variable a

--- Variable.java
+++ Variable.java
@@ -1,6 +1,13 @@
 public class Variable {
     public static void main(String[] args) {
+        // one way
+
         String string = new String("This is a string");
-        System.out.println(string);
+        System.out.println(string + ":)");
+        // string + ":)" known as the expression
+        String easier = "Wow, that was easy!";
+        System.out.println(easier);
+
     }
+
 }
