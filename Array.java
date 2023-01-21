670ac509b4a514e3f70085252aac871c01235371
Pheak02 <email@gmail.com>
2023-01-21 19:49:19 +0700

    For loop in array

--- Array.java
+++ Array.java
@@ -2,14 +2,19 @@ import java.util.Arrays;
 
 public class Array {
     public static void main(String[] args) {
-        // int[] grades = new int[10];
+        int[] grades = new int[10];
         // grades[0] = 10;
-        int[] grades = { 10, 7, 8, 4, 5, 2, 9 };
+        // int[] grades = { 10, 7, 8, 4, 5, 2, 9 };
         // overwrite array element
         // grades[0] = 900;
-        System.out.println(Arrays.toString(grades));
+        // System.out.println(Arrays.toString(grades));
 
-        int[][] grades1 = { { 7, 8, 4 }, { 5, 2, 9 } };
-        System.out.println(Arrays.deepToString(grades1));
+        // array inside array
+        // int[][] grades1 = { { 7, 8, 4 }, { 5, 2, 9 } };
+        // System.out.println(Arrays.deepToString(grades1));
+        for (int i = 0; i < 10; i++) {
+            grades[i] = 5;
+        }
+        System.out.println(Arrays.toString(grades));
     }
 }
