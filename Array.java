ed63d8aeb338519cb9212df24055ee6b86f22d33
Pheak02 <email@gmail.com>
2023-01-21 17:01:25 +0700

    overwrite array element

--- Array.java
+++ Array.java
@@ -3,7 +3,7 @@ public class Array {
         // int[] grades = new int[10];
         // grades[0] = 10;
         int[] grades = { 10, 7, 8, 4, 5, 2, 9 };
-        // overrite array element
+        // overwrite array element
         grades[0] = 900;
         System.out.println(grades[0]);
     }
