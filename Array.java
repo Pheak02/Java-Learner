af146e7fe4bcfedfa99ca48fdba0b4c3fdbfd6ec
Pheak02 <email@gmail.com>
2023-01-21 17:09:12 +0700

    Print out arrays with toString

--- Array.java
+++ Array.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 public class Array {
     public static void main(String[] args) {
         // int[] grades = new int[10];
         // grades[0] = 10;
         int[] grades = { 10, 7, 8, 4, 5, 2, 9 };
         // overwrite array element
-        grades[0] = 900;
-        System.out.println(grades[0]);
+        // grades[0] = 900;
+        System.out.println(Arrays.toString(grades));
     }
 }
