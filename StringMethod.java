cb9d2ce3aa835d3e8762e36a3ec7870f8f138fb1
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 19:19:43 +0700

    indexOf

--- StringMethod.java
+++ StringMethod.java
@@ -1,7 +1,9 @@
 public class StringMethod {
     public static void main(String[] args) {
         String mainStatement = "Java is Fun as Java is boring";
-        System.out.println(mainStatement.indexOf('Java'));
+        System.out.println(mainStatement.indexOf('a'));
+        // may have to slit the word of string first
+        System.out.println(mainStatement.lastIndexOf('a'));
 
     }
 }
