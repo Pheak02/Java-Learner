7630a5c7a1e0860ee5533731099f0a2f52b3b2d6
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 21:21:07 +0700

    all good

--- User_Defined_Method/MethodModifier.java
+++ User_Defined_Method/MethodModifier.java
@@ -2,12 +2,11 @@ package User_Defined_Method;
 
 public class MethodModifier {
     public static void main(String[] args) {
-        MethodModifier obj = new MethodModifier();
-        obj.add(3, 4);
+        add(3, 4);
 
     }
 
-    public void add(int a, int b) { // won't be a class - can't be call to use
+    public static void add(int a, int b) { // won't be a class - can't be call to use
         int c = a + b;
         System.out.println(c);
     }
