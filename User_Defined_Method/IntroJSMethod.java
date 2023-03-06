94b2c20efd295895d49716c27f45e0549d27d499
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 21:09:18 +0700

    use main method with user definded method

---
+++ User_Defined_Method/IntroJSMethod.java
@@ -0,0 +1,17 @@
+package User_Defined_Method;
+
+public class IntroJSMethod {
+    public static void main(String[] args) {
+        add();
+        add();
+        add();
+
+    }
+
+    public static void add() {
+        int a = 5;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
