0a7fb89e4af059f5cd3f117636aa04a04244afa7
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 17:54:48 +0700

    indexOfSth

---
+++ StringMethod.java
@@ -0,0 +1,7 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String mainStatement = "Java is Fun as Java is boring";
+        System.out.println(mainStatement.indexOf('Java'));
+
+    }
+}
