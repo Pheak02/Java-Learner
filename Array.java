90c41e9012175062e2612ecbe5e100a98380cbe0
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 17:54:36 +0700

    commit

--- Array.java
+++ Array.java
@@ -1,20 +1,39 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
+// public class Array {
+//     public static void main(String[] args) {
+//         int[] grades = new int[10];
+//         Scanner input = new Scanner(System.in);
+//         // grades[0] = 10;
+//         // int[] grades = { 10, 7, 8, 4, 5, 2, 9 };
+//         // overwrite array element
+//         // grades[0] = 900;
+//         // System.out.println(Arrays.toString(grades));
+
+//         // array inside array
+//         // int[][] grades1 = { { 7, 8, 4 }, { 5, 2, 9 } };
+//         // System.out.println(Arrays.deepToString(grades1));
+//         for (int i = 0; i < 10; i++) {
+//             int x = input.nextInt();
+//             grades[i] = x;
+//         }
+//         System.out.println(Arrays.toString(grades));
+//     }
+// }
+
+// input with the size of the arrays
 public class Array {
     public static void main(String[] args) {
-        int[] grades = new int[10];
-        // grades[0] = 10;
-        // int[] grades = { 10, 7, 8, 4, 5, 2, 9 };
-        // overwrite array element
-        // grades[0] = 900;
-        // System.out.println(Arrays.toString(grades));
+        try (Scanner input = new Scanner(System.in)) {
+            int size = input.nextInt();
+            int[] grades = new int[size];
 
-        // array inside array
-        // int[][] grades1 = { { 7, 8, 4 }, { 5, 2, 9 } };
-        // System.out.println(Arrays.deepToString(grades1));
-        for (int i = 0; i < 10; i++) {
-            grades[i] = 5;
+            for (int i = 0; i < size; i++) {
+                int x = input.nextInt();
+                grades[i] = x;
+            }
+            System.out.println(Arrays.toString(grades));
         }
-        System.out.println(Arrays.toString(grades));
     }
 }
---
+++ PrimitivesAndObject.java
@@ -0,0 +1,6 @@
+public class PrimitiveAndObject{
+    public static void main(String[] args){
+        String string = new String("This is a string");
+        String easier = 
+    }
+}
