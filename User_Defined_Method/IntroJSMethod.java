4f0c01f0f68a2f12b555b84e57b8d3f790fb8468
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 21:12:06 +0700

    pass in the parameters inside the method

--- User_Defined_Method/IntroJSMethod.java
+++ User_Defined_Method/IntroJSMethod.java
@@ -2,15 +2,13 @@ package User_Defined_Method;
 
 public class IntroJSMethod {
     public static void main(String[] args) {
-        add();
-        add();
-        add();
+        add(3, 4);
+        add(13, 1232);
+        add(2, 3);
 
     }
 
-    public static void add() {
-        int a = 5;
-        int b = 2;
+    public static void add(int a, int b) {
         int c = a + b;
         System.out.println(c);
     }
