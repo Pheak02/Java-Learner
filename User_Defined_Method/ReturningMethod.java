b076fd381706ebc58e767840eaabca7d1490820e
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 21:46:23 +0700

    return method

---
+++ User_Defined_Method/ReturningMethod.java
@@ -0,0 +1,12 @@
+package User_Defined_Method;
+
+public class ReturningMethod {
+    public static void main(String[] args) {
+        System.out.println(add(2, 5));
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+}
