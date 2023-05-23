fa30ad251bc43d3bdaac8f674ea4c7b3bf1b23f2
Pheak02 <saingsopheak02@gmail.com>
2023-05-23 11:55:14 +0700

    primitive type number

--- Explore/coreConcept.java
+++ Explore/coreConcept.java
@@ -1,11 +1,16 @@
 package Explore;
 
 public class coreConcept {
-    public static void main( String[] args) {
-        int a=4;
-        char b= 'B';
-        long c=400;
-        double d=3.2;
-        
+    public static void main(String[] args) {
+        // Primitive Type number - turn blue
+        int a = 4;
+        char b = 'B';
+        long c = 400;
+        double d = 3.2;
+        // non-primitive type number - not turn blue - means it's an object
+        String name = "Meisme";
+        // when don't have sort of (), put ;
+        System.out.println(name.toUpperCase()); // use with method()
+
     }
 }
