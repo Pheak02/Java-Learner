11e467ea98b431c37f7fd29f54e5067444cdf958
Pheak02 <saingsopheak02@gmail.com>
2023-05-23 12:52:38 +0700

    add on arraylist<>

--- ArrayList/ArrayVsArrayList.java
+++ ArrayList/ArrayVsArrayList.java
@@ -1,16 +1,18 @@
 package ArrayList;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ArrayVsArrayList {
     public static void main(String[] args) {
-        String[] frieldArray1 = new String[4]; // it has the fix size
-        String[] frieldArray2 = { "Jonh", "Chris", "Eric", "Luke" }; // declear size by implied its elements
-        ArrayList<String> friendsArrayList = new ArrayList<>(); // has dynamic size
-        ArrayList<String> friendsArrayList2 = new ArrayList<>(Arrays.asList("Jonh", "Chris", "Eric", "Luke"));
-        System.out.println(frieldArray2[1]);
-        System.out.println(friendsArrayList2.get(1));
+        // String[] frieldArray1 = new String[4]; // it has the fix size
+        // String[] frieldArray2 = { "Jonh", "Chris", "Eric", "Luke" }; // declear size
+        // by implied its elements
+        // ArrayList<String> friendsArrayList = new ArrayList<>(); // has dynamic size
+        // ArrayList<String> friendsArrayList2 = new ArrayList<>(Arrays.asList("Jonh",
+        // "Chris", "Eric", "Luke"));
+        // System.out.println(frieldArray2[1]);
+        // System.out.println(friendsArrayList2.get(1));
+        ArrayList<Integer> a = new ArrayList<Integer>();
 
     }
 
