44fb827fd30bf78338967360cab40317e61b7149
Pheak02 <saingsopheak02@gmail.com>
2023-05-23 11:45:57 +0700

    store primitive type number

---
+++ Explore/coreConcept.java
@@ -0,0 +1,11 @@
+package Explore;
+
+public class coreConcept {
+    public static void main( String[] args) {
+        int a=4;
+        char b= 'B';
+        long c=400;
+        double d=3.2;
+        
+    }
+}
