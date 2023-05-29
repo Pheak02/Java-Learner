49ba8499b82e51157bb7189f76634acc8f59c834
Pheak02 <saingsopheak02@gmail.com>
2023-05-29 21:37:25 +0700

    define data type

---
+++ basic/dataTypes.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class dataTypes {
+    public static void main(String[] arg) {
+        int myNum = 5; // Integer (whole number)
+        float myFloatNum = 5.99f; // Floating point number
+        char myLetter = 'D'; // Character
+        boolean myBool = true; // Boolean
+        String myText = "Hello"; // String
+
+    }
+
+}
