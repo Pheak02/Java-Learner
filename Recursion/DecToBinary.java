3b64a4d1542c9afc89a1e9bd62127a37c486b795
Pheak02 <saingsopheak02@gmail.com>
2023-05-31 21:43:22 +0700

    recursion with number
    Decimal to Binary

---
+++ Recursion/DecToBinary.java
@@ -0,0 +1,14 @@
+package Recursion;
+public class DecToBinary {
+    public static void main(String args[]) {
+        String binary = findBinary(233, "");
+    }
+
+    public static String findBinary(int decimal, String result) {
+        if (decimal == 0) {
+            return result;
+        }
+        result = decimal % 2 + result;
+        return findBinary(decimal / 2, result);
+    }
+}
