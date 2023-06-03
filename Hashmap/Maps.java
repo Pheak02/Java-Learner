1ac8ca6b25c60d28fcfdea3c5bd700c1b37c3d8f
Sopheak Saing <saingsopheak02@gmail.com>
2023-06-03 11:23:57 +0700

    create map with string to integer

---
+++ Hashmap/Maps.java
@@ -0,0 +1,14 @@
+package Hashmap;
+
+import java.util.HashMap;
+
+public class Maps {
+    public static void main(String[] args) {
+        HashMap<String, Integer> empIds = new HashMap<>(); // map of str to int
+        empIds.put("Meisme", 01);
+        empIds.put("Dnao", 02);
+        empIds.put("kalo", 03);
+
+
+    }
+}
