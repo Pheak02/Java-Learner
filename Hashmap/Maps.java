4cd7b5a1c44be3447b5331db085c30398edd1972
Sopheak Saing <saingsopheak02@gmail.com>
2023-06-03 15:19:13 +0700

    Add elements to a HashMap

--- Hashmap/Maps.java
+++ Hashmap/Maps.java
@@ -4,24 +4,15 @@ import java.util.HashMap;
 
 public class Maps {
     public static void main(String[] args) {
-        HashMap<String, Integer> empIds = new HashMap<>(); // map of str to int
-        empIds.put("Meisme", 01);
-        empIds.put("Dnao", 02);
-        empIds.put("kalo", 03);
-        System.out.println(empIds);
-        System.out.println(empIds.get("kalo"));
-        System.out.println(empIds.containsValue(01));
-        System.out.println(empIds.containsKey("sopheak"));
-        // OVERWRITE OLD VALUE
-        empIds.put("kalo", 04);
-        System.out.println(empIds);
-        empIds.putIfAbsent("Jonh", 222);
-        System.out.println(empIds);
+        // create a hashmap
+        HashMap<String, Integer> numbers = new HashMap<>();
 
-        empIds.putIfAbsent("Thida", 2525);
-        System.out.println(empIds);
-        empIds.remove("Thida", 2525);
-        System.out.println(empIds);
+        System.out.println("Initial HashMap: " + numbers);
+        // put() method to add elements
+        numbers.put("One", 1);
+        numbers.put("Two", 2);
+        numbers.put("Three", 3);
+        System.out.println("HashMap after put(): " + numbers);
 
     }
 }
