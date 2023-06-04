7d3c498978330cb3e9e1296fe2bae1b83ffb16a0
Sopheak Saing <saingsopheak02@gmail.com>
2023-06-04 17:51:15 +0700

    Compute Employee Working Hour
    create hashmap & printout format correctly

--- employeeHour.java
+++ employeeHour.java
@@ -8,14 +8,16 @@
 
 public class employeeHour {
     public static void main(String[] args) {
-        int[][] employeeHours = { { 2, 4, 3, 4, 5, 8, 8 },
+        int[][] employeeHours = {
+                { 2, 4, 3, 4, 5, 8, 8 },
                 { 7, 3, 4, 3, 3, 4, 4 },
                 { 3, 3, 4, 3, 3, 2, 2 },
                 { 9, 3, 4, 7, 3, 4, 1 },
                 { 3, 5, 4, 3, 6, 3, 8 },
                 { 3, 4, 4, 6, 3, 4, 4 },
                 { 3, 7, 4, 8, 3, 8, 4 },
-                { 6, 3, 5, 9, 2, 7, 9 } };
+                { 6, 3, 5, 9, 2, 7, 9 }
+        };
 
         // int[][] totalHoursPerEmployee = new int[7][1];
         int[] totalHoursPerEmployee = sumEmployeeHours(employeeHours);
