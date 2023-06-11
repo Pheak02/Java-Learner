a621304a965626749266e1012b538b784c523651
Sopheak Saing <saingsopheak02@gmail.com>
2023-06-11 13:49:55 +0700

    input & output in java

---
+++ output.java
@@ -0,0 +1,34 @@
+public class output {
+    public static void main(String[] args) {
+
+        double num1 = -10.6;
+
+        System.out.println("1. println ");
+        System.out.println("2. println ");
+
+        System.out.print("1. print ");
+        System.out.print("2. print");
+
+        System.out.printf("1. printf ");
+        System.out.printf("2. printf");
+
+        System.out.println("Java is " + "awesome.");
+        System.out.println("num1 is " + num1);
+        System.out.println("The first aphabet is " + 'a');
+        System.out.print("2 * 5 is " + 2 * 5 + "\n");
+
+        char c = 'a';
+        int n = 10;
+        float f = 2.0f;
+        double d = 5.5;
+        String s = "Hello";
+        boolean b = true;
+        System.out.printf("c = %c\n", c);
+        System.out.printf("n = %d\n", n);
+        System.out.printf("f = %.2f\n", f);
+        System.out.printf("d = %.2f\n", d);
+        System.out.printf("s = %s\n", s);
+        System.out.printf("b = %b\n", b);
+    }
+
+}
