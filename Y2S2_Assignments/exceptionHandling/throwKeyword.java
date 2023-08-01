5a1943264b5d7a8c8d5d75c60fe8b1db5a69553a
Sopheak Saing <saingsopheak02@gmail.com>
2023-08-01 08:55:21 +0700

    throw keyword

---
+++ Y2S2_Assignments/exceptionHandling/throwKeyword.java
@@ -0,0 +1,10 @@
+public class throwKeyword {
+
+    public static void divideByZero() {
+        throw new ArithmeticException("Trying to divide by 0");
+    }
+
+    public static void main(String[] args) {
+        divideByZero();
+    }
+}
