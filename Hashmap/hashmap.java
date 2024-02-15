f9aa1068e2f2efb1d4cd99e53ed40b00c3178270
Pheak02 <saingsopheak02@gmail.com>
2024-02-15 12:38:29 +0700

    hash map, creaate key-value pair

---
+++ Hashmap/hashmap.java
@@ -0,0 +1,11 @@
+
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> students = new HashMap<>();
+        hashMap.put("1", "Sopheak");
+        hashMap.put("2", "Lis");
+
+    }
+
+}
