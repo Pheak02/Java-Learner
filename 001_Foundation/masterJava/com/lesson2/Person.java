c063fe4809fa6cdaf5d1f3f5ebf430ff7ea81b63
Pheak02 <saingsopheak02@gmail.com>
2025-08-04 23:25:42 +0700

    restructure and add new learning path 001

---
+++ 001_Foundation/masterJava/com/lesson2/Person.java
@@ -0,0 +1,21 @@
+package masterJava.com.lesson2; //must unique, consist of domain name
+
+import masterJava.com.lesson3.Name;//import from other module/package
+public class Person {
+
+    private Name personName;
+//    private static Name personName; //static class here will change all other instance(personName) as well.
+    public Person(Name personName) {
+        this.personName = personName;
+    }
+
+    public Person() {
+    /*
+    empty on purpose - default constructor
+    */
+    }
+
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
---
+++ 001_Foundation/masterJava/com/lesson3/Name.java
@@ -0,0 +1,4 @@
+package masterJava.com.lesson3;
+
+public class Name {
+}
